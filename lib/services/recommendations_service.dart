5702c12c7bf5cdfa65eb3c393bc206aa655cb757
Hezekiah <38362531+Hezeh@users.noreply.github.com>
2021-06-01 23:38:20 +0300

    Added location in query

--- lib/services/recommendations_service.dart
+++ lib/services/recommendations_service.dart
@@ -4,16 +4,34 @@ import 'package:beammart/models/item_recommendations.dart';
 import 'package:http/http.dart' as http;
 
 Future<ItemRecommendations> getRecs({double? lat, double? lon}) async {
-  final response = await http.get(
-    Uri(
-      scheme: 'https',
-      host: 'api.beammart.app',
-      path: 'recs' 
-    ),
-  );
-  final jsonResponse = ItemRecommendations.fromJson(json.decode(response.body));
-  if (response.statusCode == 200) {
-    return jsonResponse;
+  if (lat != null && lon != null) {
+    final response = await http.get(
+      Uri(
+        scheme: 'https',
+        host: 'api.beammart.app',
+        path: 'recs',
+        query: 'lat=$lat&lon=$lon'
+      ),
+    );
+    final jsonResponse =
+        ItemRecommendations.fromJson(json.decode(response.body));
+    if (response.statusCode == 200) {
+      return jsonResponse;
+    }
+    return ItemRecommendations();
+  } else {
+    final response = await http.get(
+      Uri(
+        scheme: 'https',
+        host: 'api.beammart.app',
+        path: 'recs',
+      ),
+    );
+    final jsonResponse =
+        ItemRecommendations.fromJson(json.decode(response.body));
+    if (response.statusCode == 200) {
+      return jsonResponse;
+    }
+    return ItemRecommendations();
   }
-  return ItemRecommendations();
 }
